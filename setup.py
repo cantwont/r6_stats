4f8f08168e3062e775fc3d95423dd3c5da9847b0
cantwont <149845660+cantwont@users.noreply.github.com>
2023-11-03 18:48:22 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name="r6_stats",
-    version="1.0.0",
+    version="1.0.2",
     packages=["r6_stats"],
-    install_requires=["requests", "bs4", "lxml"],
-    description="Python package to retreive player stats from the game Rainbow 6: Siege"
+    install_requires=["requests", "beautifulsoup4", "lxml"],
+    description="Python package to retreive player stats from the game Rainbow 6: Siege",
+    long_description="Python package to retreive player stats from the game Rainbow 6: Siege"
 )
